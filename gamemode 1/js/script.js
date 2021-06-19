0ae32fc8c45abec663268365a800785a5b13893e
olexo <olexo884@gmail.com>
2021-06-19 11:34:25 +0300

    Add all my files

---
+++ gamemode 1/js/script.js
@@ -0,0 +1,10 @@
+$('.burger_menu_btn').click(function() {
+    $('.burger_menu__nav').css({
+        'display': 'block'
+    });
+});
+$('#burger_btn').click(function() {
+    $('.burger_menu__nav').css({
+        'display': 'none'
+    });
+});
